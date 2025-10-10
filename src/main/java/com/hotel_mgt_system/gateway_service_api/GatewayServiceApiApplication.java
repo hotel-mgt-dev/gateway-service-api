076ba66f3b148124a5690a7c76685031d8d2e7a0
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-10 12:14:41 +0530

    init

---
+++ src/main/java/com/hotel_mgt_system/gateway_service_api/GatewayServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.hotel_mgt_system.gateway_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApiApplication.class, args);
+	}
+
+}
