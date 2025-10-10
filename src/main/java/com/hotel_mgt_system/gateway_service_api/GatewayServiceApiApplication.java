9943ac260af9f8c3be1069780617e3071d1666dc
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-10 12:28:50 +0530

    add application properties

--- src/main/java/com/hotel_mgt_system/gateway_service_api/GatewayServiceApiApplication.java
+++ src/main/java/com/hotel_mgt_system/gateway_service_api/GatewayServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.hotel_mgt_system.gateway_service_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServiceApiApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/hotel_mgt_system/gateway_service_api/GatewayServiceApiApplicationTests.java
+++ src/test/java/com/hotel_mgt_system/gateway_service_api/GatewayServiceApiApplicationTests.java
@@ -2,10 +2,8 @@ package com.hotel_mgt_system.gateway_service_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootTest
-@EnableDiscoveryClient
 class GatewayServiceApiApplicationTests {
 
 	@Test
