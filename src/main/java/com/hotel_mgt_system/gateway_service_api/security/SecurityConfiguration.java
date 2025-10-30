7d90378f7e4aab500b74a856754bb5c5bdb37ca0
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-31 04:16:37 +0530

    add gate service security config class

--- src/main/java/com/hotel_mgt_system/gateway_service_api/security/SecurityConfiguration.java
+++ src/main/java/com/hotel_mgt_system/gateway_service_api/security/SecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
@@ -36,6 +38,31 @@ public class SecurityConfiguration {
         return  httpSecurity.build();
     }
 
+    ////// In Depriciation of jwt can use below code insted of above //////
+
+
+
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+//        httpSecurity
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchange -> exchange
+//                        .pathMatchers("/user-service/api/v1/**").permitAll()
+//                        .pathMatchers("/hotel-management/api/v1/**").permitAll()
+//                        .anyExchange()
+//                        .authenticated())
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(jwt -> jwt.jwtDecoder(jwtDecoder())));  // Non-deprecated way
+//
+//        return httpSecurity.build();
+//    }
+//
+//    @Bean
+//    public ReactiveJwtDecoder jwtDecoder() {
+//        return NimbusReactiveJwtDecoder.withIssuerLocation(issuerUri).build();
+//    }
+
+
     //set whow can send api req
 
     @Bean
@@ -50,8 +77,8 @@ public class SecurityConfiguration {
 
         UrlBasedCorsConfigurationSource basedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         basedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsWebFilter(basedCorsConfigurationSource);
+        return  new CorsWebFilter(basedCorsConfigurationSource);
 
-    }
+        }
 
 }
